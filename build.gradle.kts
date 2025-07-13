3369d6fa406f78ab7acb1ebeeb5719351cd92943
kyze8439690 <me@yanghui.name>
2025-07-13 23:31:59 +0800

    update dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,12 +4,11 @@ plugins {
 }
 
 android {
-    compileSdk = 34
-    buildToolsVersion = "34.0.0"
+    compileSdk = 36
+    buildToolsVersion = "36.0.0"
 
     defaultConfig {
         minSdk = 21
-        targetSdk = 34
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
@@ -17,25 +16,25 @@ android {
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     namespace = "com.maxmind.geoip2"
 }
 
 dependencies {
     api(project(":MaxMind-DB-Reader-java"))
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
-    implementation("commons-codec:commons-codec:1.17.1")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.1")
-    implementation("com.fasterxml.jackson.core:jackson-core:2.18.1")
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.18.1")
-    implementation("com.google.guava:guava:33.3.1-android")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
+    implementation("commons-codec:commons-codec:1.18.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.1")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.19.1")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.19.1")
+    implementation("com.google.guava:guava:33.4.8-android")
     testImplementation("junit:junit:4.13.2")
     testImplementation("com.github.tomakehurst:wiremock:3.0.1")
     testImplementation("org.skyscreamer:jsonassert:1.5.3")
-    testImplementation("org.slf4j:slf4j-simple:2.0.16")
-    testImplementation("com.fasterxml.jackson.jr:jackson-jr-objects:2.18.1")
+    testImplementation("org.slf4j:slf4j-simple:2.0.17")
+    testImplementation("com.fasterxml.jackson.jr:jackson-jr-objects:2.19.1")
     testImplementation("pl.pragmatists:JUnitParams:1.1.1")
-    testImplementation("com.jcabi:jcabi-matchers:1.7.0")
+    testImplementation("com.jcabi:jcabi-matchers:1.8.0")
 }
